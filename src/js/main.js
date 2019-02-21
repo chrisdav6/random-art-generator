0286ef22f5061395911730a619f3a6556090c909
Chris Davis <chris.davis5440@gmail.com>
2019-02-21 09:08:34 -0500

    set timeout on icon creation so that artboard clears first

--- src/js/main.js
+++ src/js/main.js
@@ -7,42 +7,42 @@ $(function () {
     return $angle;
   }
 
-  function createIcons(icon) {
-    for (var i = 0; i < 300; i++) {
-      $artboard.append(`<div class="box" style="display: flex; justify-content: center; align-items: center;"><i class='${icon}' data-fa-transform="rotate-${generateAngle()}"></i></div>`);
-    };
+  function loadArtboard(icon) {
+    $artboard.empty();
+
+    setTimeout(() => {
+      for (var i = 0; i < 300; i++) {
+        $artboard.append(`<div class="box" style="display: flex; justify-content: center; align-items: center;"><i class='${icon}' data-fa-transform="rotate-${generateAngle()}"></i></div>`);
+      };
+    }, 4);
   }
 
-  createIcons("fab fa-angellist fa-2x");
+  loadArtboard("fab fa-angellist fa-2x");
 
   $(".icon").on("click", function() {
     if ($(this).hasClass('option1')) {
-      $artboard.html("");
-      createIcons("fab fa-angellist fa-2x");
+      loadArtboard("fab fa-angellist fa-2x");
     }
     if($(this).hasClass('option2')) {
-      $artboard.html("");
       createIcons("fas fa-adjust fa-2x");
     }
     if ($(this).hasClass('option3')) {
-      $artboard.html("");
       createIcons("fas fa-angle-double-up fa-2x");
     }
     if ($(this).hasClass('option4')) {
-      $artboard.html("");
       createIcons("far fa-angry fa-2x");
     }
     if ($(this).hasClass('option5')) {
-      $artboard.html("");
       createIcons("fas fa-arrows-alt-h fa-2x");
     }
     if ($(this).hasClass('option6')) {
-      $artboard.html("");
       createIcons("fab fa-bitbucket fa-2x");
     }
     if ($(this).hasClass('option7')) {
-      $artboard.html("");
       createIcons("fas fa-child fa-2x");
+    }
+    if ($(this).hasClass('option8')) {
+      createIcons("fas fa-wrench fa-2x");
     }
   });
 
