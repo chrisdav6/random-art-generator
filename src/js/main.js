f87c65f3f858ae88c589ecefdc80cf1553b5803c
Chris Davis <chris.davis5440@gmail.com>
2019-02-21 09:21:43 -0500

    using fontawesome web fonts with css and rotating grid div rather than using fontawesome js version for a huge increase in performance

--- src/js/main.js
+++ src/js/main.js
@@ -10,11 +10,9 @@ $(function () {
   function loadArtboard(icon) {
     $artboard.empty();
 
-    setTimeout(() => {
-      for (var i = 0; i < 300; i++) {
-        $artboard.append(`<div class="box" style="display: flex; justify-content: center; align-items: center;"><i class='${icon}' data-fa-transform="rotate-${generateAngle()}"></i></div>`);
-      };
-    }, 4);
+    for (var i = 0; i < 300; i++) {
+      $artboard.append(`<div class="box" style="display: flex; justify-content: center; align-items: center; transform: rotate(${generateAngle()}deg);"><i class='${icon}'}"></i></div>`);
+    };
   }
 
   loadArtboard("fab fa-angellist fa-2x");
@@ -24,25 +22,25 @@ $(function () {
       loadArtboard("fab fa-angellist fa-2x");
     }
     if($(this).hasClass('option2')) {
-      createIcons("fas fa-adjust fa-2x");
+      loadArtboard("fas fa-adjust fa-2x");
     }
     if ($(this).hasClass('option3')) {
-      createIcons("fas fa-angle-double-up fa-2x");
+      loadArtboard("fas fa-angle-double-up fa-2x");
     }
     if ($(this).hasClass('option4')) {
-      createIcons("far fa-angry fa-2x");
+      loadArtboard("far fa-angry fa-2x");
     }
     if ($(this).hasClass('option5')) {
-      createIcons("fas fa-arrows-alt-h fa-2x");
+      loadArtboard("fas fa-arrows-alt-h fa-2x");
     }
     if ($(this).hasClass('option6')) {
-      createIcons("fab fa-bitbucket fa-2x");
+      loadArtboard("fab fa-bitbucket fa-2x");
     }
     if ($(this).hasClass('option7')) {
-      createIcons("fas fa-child fa-2x");
+      loadArtboard("fas fa-child fa-2x");
     }
     if ($(this).hasClass('option8')) {
-      createIcons("fas fa-wrench fa-2x");
+      loadArtboard("fas fa-wrench fa-2x");
     }
   });
 
