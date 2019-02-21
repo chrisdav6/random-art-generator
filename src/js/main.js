70c9dac9dc54a7c0d09cf865d7629bc09c7185f0
Chris Davis <chris.davis5440@gmail.com>
2019-02-21 08:31:44 -0500

    added font-awesome locally, made some tweaks on canvas size

--- src/js/main.js
+++ src/js/main.js
@@ -9,40 +9,40 @@ $(function () {
 
   function createIcons(icon) {
     for (var i = 0; i < 300; i++) {
-      $artboard.append(`<div><i class='${icon}' data-fa-transform="rotate-${generateAngle()}"></i></div>`);
+      $artboard.append(`<div class="box" style="display: flex; justify-content: center; align-items: center;"><i class='${icon}' data-fa-transform="rotate-${generateAngle()}"></i></div>`);
     };
   }
 
-  createIcons("fab fa-angellist");
+  createIcons("fab fa-angellist fa-2x");
 
   $(".icon").on("click", function() {
     if ($(this).hasClass('option1')) {
       $artboard.html("");
-      createIcons("fab fa-angellist");
+      createIcons("fab fa-angellist fa-2x");
     }
     if($(this).hasClass('option2')) {
       $artboard.html("");
-      createIcons("fas fa-adjust");
+      createIcons("fas fa-adjust fa-2x");
     }
     if ($(this).hasClass('option3')) {
       $artboard.html("");
-      createIcons("fas fa-angle-double-up");
+      createIcons("fas fa-angle-double-up fa-2x");
     }
     if ($(this).hasClass('option4')) {
       $artboard.html("");
-      createIcons("far fa-angry");
+      createIcons("far fa-angry fa-2x");
     }
-    if ($(this).hasClass('option4')) {
+    if ($(this).hasClass('option5')) {
       $artboard.html("");
-      createIcons("fas fa-arrows-alt-h");
+      createIcons("fas fa-arrows-alt-h fa-2x");
     }
     if ($(this).hasClass('option6')) {
       $artboard.html("");
-      createIcons("fab fa-bitbucket");
+      createIcons("fab fa-bitbucket fa-2x");
     }
     if ($(this).hasClass('option7')) {
       $artboard.html("");
-      createIcons("fas fa-child");
+      createIcons("fas fa-child fa-2x");
     }
   });
 
